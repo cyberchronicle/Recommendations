32fae05e69d6875d45a4a82e2c828c4f73babd3f
GashaSusev <gavlogin3@gmail.com>
2024-12-06 11:24:38 +0300

    enh: tests

--- tests/run_integration_tests.sh
+++ tests/run_integration_tests.sh
@@ -6,7 +6,8 @@ if [ $status_add -ne 0 ]; then
   echo "Ошибка при вызове /add_article: $response_add"
   exit 1
 else
-  echo "Успешный вызов /add_article. Ответ: $response_add"
+  echo "Успешный вызов /add_article. Ответ: $response_add "
+  echo " "
 fi
 
 # Тест /search_by_keyword
@@ -21,9 +22,105 @@ else
   grep -q '1' <<< "$response_search" 
   if [ $? -eq 0 ]; then
     echo "ids=1 найден в ответе /search_by_keyword"
+    echo " "
   else
     echo "Предупреждение: ids=1 не найден в ответе /search_by_keyword. Проверьте данные."
+    echo " "
   fi
+fi
+
+
+#Suggest
+echo "Тестирование suggest 1/2"
+RESPONSE=$(curl -s -X POST -H "accept: application/json" -H "Content-Type: application/json" -d '{ "user_tags": ["string"], "articles": [{"id": "1", "tags": ["string"]}] }' http://localhost:8001/suggest/1)
+STATUS=$?
+
+if [ $STATUS -ne 0 ]; then
+  echo "Ошибка при вызове /suggest: $RESPONSE"
+  exit 1
+fi
+
+EXPECTED_OUTPUT='{"ids":["1"]}'
+
+if [[ "$RESPONSE" == "$EXPECTED_OUTPUT" ]]; then
+  echo "Тест /suggest пройден успешно"
+  echo " "
+else
+  echo "Тест /suggest провален. Полученный результат: $RESPONSE"
+  echo " "
+fi
+
+echo "Тестирование suggest 2/2"
+RESPONSE=$(curl -s -X POST -H "accept: application/json" -H "Content-Type: application/json" \
+  -d '{
+    "user_tags": ["осень", "небо"],
+    "articles": [
+      {
+        "id": "1",
+        "tags": ["осень", "небо"]
+      },
+      {
+        "id": "2",
+        "tags": ["весна", "земля"]
+      }
+    ]
+  }' http://localhost:8001/suggest/1)
+
+EXPECTED_OUTPUT='{"ids":["1"]}'
+if [[ "$RESPONSE" == "$EXPECTED_OUTPUT" ]]; then
+  echo "Тест /suggest: Успешный ответ пройден"
+  echo " "
+else
+  echo "Тест /suggest: Успешный ответ провален. Получено: $RESPONSE"
+  echo " "
+fi
+
+
+
+#Text Process
+echo "Тестирование Text Process 1/3"
+RESPONSE=$(curl -s -X POST -H "accept: application/json" -H "Content-Type: application/json" -d '{ "text": "что такое осень это небо" }' http://localhost:8001/text/process)
+STATUS=$?
+
+if [ $STATUS -ne 0 ]; then
+  echo "Ошибка при вызове text process: $RESPONSE"
+  exit 1
+fi
+
+EXPECTED_OUTPUT='{"tags":["небо","осень"]}'
+
+if [[ "$RESPONSE" == "$EXPECTED_OUTPUT" ]]; then
+  echo "Тест /text/process пройден успешно"
+  echo " " 
+else
+  echo "Тест /text/process провален. Полученный результат: $RESPONSE"
+  echo " "
+fi
+
+echo "Тестирование Text Process 2/3"
+RESPONSE=$(curl -s -X POST -H "accept: application/json" -H "Content-Type: application/json" \
+  -d '{"text": ""}' http://localhost:8001/text/process)
+
+EXPECTED_OUTPUT='{"tags":[]}'
+if [[ "$RESPONSE" == "$EXPECTED_OUTPUT" ]]; then
+  echo "Тест /text/process: Пустой ввод пройден"
+  echo " "
+else
+  echo "Тест /text/process: Пустой ввод провален. Получено: $RESPONSE"
+  echo " "
+fi
+
+echo "Тестирование Text Process 3/3"
+RESPONSE=$(curl -s -X POST -H "accept: application/json" -H "Content-Type: application/json" \
+  -d '{"text": "9018038, ZZZ! йцукен)"}' http://localhost:8001/text/process)
+
+EXPECTED_OUTPUT='{"tags":[]}'
+if [[ "$RESPONSE" == "$EXPECTED_OUTPUT" ]]; then
+  echo "Тест /text/process: Текст без тегов пройден"
+  echo " "
+else
+  echo "Тест /text/process: Текст без тегов провален. Получено: $RESPONSE"
+  echo " "
 fi
 
 
